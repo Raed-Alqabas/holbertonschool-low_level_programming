d01f4ec2da08429231fddc60688e3efeb1698511
Raed-Alqabas <rayeddd55@gmail.com>
2025-04-27 14:27:34 +0000

    try1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - func check the codc
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
