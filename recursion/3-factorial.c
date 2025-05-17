98cf834c3cd640992650cd73f96e49799042ebf4
Raed-Alqabas <rayeddd55@gmail.com>
2025-05-17 09:44:50 +0000

    try4

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,1 +1,16 @@
-there's nothing we can do
+#include "main.h"
+/**
+ * factorial - factorial of a given number
+ * @n: num
+ * Return: reslut
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n < 1)
+		return (1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
