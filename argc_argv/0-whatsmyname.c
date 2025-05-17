83011322939697c166776530483918140bf2b32a
Raed-Alqabas <rayeddd55@gmail.com>
2025-05-17 12:27:24 +0000

    try2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,18 @@
-there is nothing we can do
+#include <stdio.h>
+/**
+ * main - imprime los valores de argv
+ * @argc: cantidad
+ * @argv: array
+ *
+ * Return: retorna 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
