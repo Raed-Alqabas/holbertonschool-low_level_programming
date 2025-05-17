292bcd296010cb6c25bb169593e40ad09f85dc29
Raed-Alqabas <rayeddd55@gmail.com>
2025-05-17 12:06:52 +0000

    try2

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,2 +1,15 @@
-s there anything we can do ?
-there is nothing we can do
+#include "main.h"
+/**
+ * _pow_recursion - result pow
+ * @x: num
+ * @y: pow
+ * Return: num of pow
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (_pow_recursion(x, y - 1) * x);
+}
