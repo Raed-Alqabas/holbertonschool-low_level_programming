39b01236e563fad8c577f6832244616fec284269
Raed-Alqabas <rayeddd55@gmail.com>
2025-05-17 09:44:03 +0000

    try3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,1 @@
-int factorial(int n)
-{
-	if (n < 0)
-		return (-1);
-	else if (n < 1)
-		return (1);
-	else if (n == 0)
-		return (1);
-	return (n * factorial(n - 1));
-}
+there's nothing we can do
